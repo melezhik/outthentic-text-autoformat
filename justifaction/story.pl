f629fd5396e8ddade6e06847f778a24d22e2cb8c
Alexey Melezhik <melezhik@gmail.com>
2015-11-12 17:35:19 +0300

    add justification test

---
+++ justifaction/story.pl
@@ -0,0 +1,9 @@
+use Text::Autoformat;
+
+print autoformat <<HERE, {justify => 'full', tabspace => 4};
+R3>     Now is the Winter of our discontent made
+R3> glorious Summer by this son of York. And all
+R3> the clouds that lour'd upon our house In the
+R3>              deep bosom of the ocean buried.
+HERE
+
